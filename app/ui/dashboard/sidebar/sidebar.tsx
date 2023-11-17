e43662dadea3bc7bd38ff1768bb5e2a158bdfe8c
SourceSensei <source.sensei1205@gmail.com>
2023-11-17 23:14:02 +0000

    logout update

--- app/ui/dashboard/sidebar/sidebar.tsx
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -81,7 +81,13 @@ const Sidebar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.user}>
-        <Image className={styles.userImage} src="/astronaut.png" alt="" width={50} height={50} />
+        <Image
+          className={styles.userImage}
+          src="/astronaut.png"
+          alt=""
+          width={50}
+          height={50}
+        />
         <div className={styles.userDetail}>
           <span className={styles.username}>John Doe</span>
           <span className={styles.userTitle}>Admin</span>
@@ -90,14 +96,17 @@ const Sidebar = () => {
       <ul className={styles.list}>
         {menuItems.map((cat) => (
           <li key={cat.title}>
-            <span className={styles.cat}>{cat.title}
-            </span>
+            <span className={styles.cat}>{cat.title}</span>
             {cat.list.map((item) => (
-              <MenuLink item={item} key={item.title} />  
+              <MenuLink item={item} key={item.title} />
             ))}
           </li>
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
     </div>
   );
 };
