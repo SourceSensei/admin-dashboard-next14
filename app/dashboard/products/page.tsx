8250485d10bf31f50b71807642d265c141eba9a3
SourceSensei <source.sensei1205@gmail.com>
2023-11-22 21:46:26 +0000

    users and products

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+
+
 const ProductsPage = () => {
   return (
-    <div>
-      <h1>ProductsPage</h1>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.button}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}><Image src="/noproduct.jpg" width={40} height={40} alt="" className={styles.productImage} />iPhone</div>
+            </td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>12/01/2023</td>
+            <td>72</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination/>
     </div>
   );
 };
---
+++ app/ui/dashboard/pagination/pagination.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styles from "./pagination.module.css";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const Pagination = () => {
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
+    </div>
+  )
+}
+
+export default Pagination
