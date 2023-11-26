f888f9c3c17188a610c90d72290563fc2c75112f
SourceSensei <source.sensei1205@gmail.com>
2023-11-26 18:04:48 +0000

    quick fix 5

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -2,9 +2,8 @@ import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
 import Image from "next/image";
 
 const SingleUserPage = async () => {
-  
-//   const { id } = params;
-//   const user = await fetchUser(id);
+  //   const { id } = params;
+  //   const user = await fetchUser(id);
 
   return (
     <div className={styles.container}>
@@ -16,7 +15,7 @@ const SingleUserPage = async () => {
       </div>
       <div className={styles.formContainer}>
         <form action="" className={styles.form}>
-          <input type="hidden" name="id" value="id"/>
+          <input type="hidden" name="id" value="id" />
           <label>Username</label>
           <input type="text" name="username" placeholder="John Doe" />
           <label>Email</label>
@@ -26,16 +25,19 @@ const SingleUserPage = async () => {
           <label>Phone</label>
           <input type="text" name="phone" placeholder="123-456-7890" />
           <label>Address</label>
-          <textarea name="address" placeholder="123 Main St. Anytown, USA 12345" />
+          <textarea
+            name="address"
+            placeholder="123 Main St. Anytown, USA 12345"
+          />
           <label>Is Admin?</label>
           <select name="isAdmin" id="isAdmin">
-            <option value={true} >Yes</option>
-            <option value={false} >No</option>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
           </select>
           <label>Is Active?</label>
           <select name="isActive" id="isActive">
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
           </select>
           <button>Update</button>
         </form>
